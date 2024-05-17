c5030867fa490ca06c2bb46052dd3d24e19ae9db
butterfalls <1731860884@qq.com>
2024-05-17 17:30:41 +0800

    备注

---
+++ src/print.h
@@ -0,0 +1,18 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-05-16 19:45:51
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-05-17 09:15:08
+ * @FilePath: \code\Make\print.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef _PRINT_H
+#define _PRINT_H
+
+#include <stdio.h>
+
+void print();
+
+void print1();
+
+#endif
